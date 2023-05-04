54bf5132af5b9b135ac76c5cf8de4f41624f8b70
Limchaeeun914 <dlacodms914@naver.com>
2023-05-04 16:13:36 +0900

    2023/05/04 main.py 구현

--- main.py
+++ main.py
@@ -1,9 +1,28 @@
-from flask import Flask, redirect,render_template,request
-
-app=Flask(__name__)
-
-
-
-
-if __name__=='__main__':
-    app.run()
+from flask import Flask, redirect,render_template,request 
+ 
+app=Flask(__name__) 
+ 
+result_list = list()#추가부분 
+header = ['Name', 'Number', 'Major', 'Email', 'Gender', 'Languages']#추가부분 
+ 
+@app.route('/', methods = ['GET', 'POST'])  #추가부분 
+def main():#추가부분 
+   return render_template('main.html')#추가부분 
+ 
+#추가부분 
+@app.route('/result', methods = ['POST','GET']) 
+def result(): 
+   if request.method == 'POST': 
+      result = dict()
+      result['Name'] = request.form.get('name') 
+      result['Number'] = request.form.get('number') 
+      result['Major'] = request.form.get('major') 
+      result['Email'] = request.form.get('email_id') + '@' + request.form.get('email_addr') 
+      result['Gender'] = request.form.get('gender') 
+      result['languages'] = ', '.join(request.form.getlist('chkbox')) 
+      result_list.append(result) 
+      return render_template("result.html", result = result_list, header = header) 
+#추가부분 
+ 
+if __name__ == '__main__': 
+   app.run('0.0.0.0', port=8000, debug = True)#변경부분
